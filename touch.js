e3924b0af32d01a9f2a58071054b02d5345d417c
Angel <1049547382@qq.com>
2018-09-19 20:48:15 +0800

    adjust touch

--- touch.js
+++ touch.js
@@ -1,82 +1,60 @@
-function touch (ele,option) {
-  ele.addEventListener("touchstart", function (e) {
-		this.startX = e.touches[0].pageX
-		this.startY = e.touches[0].pageY
+function touch(ele, option) {
+  ele.addEventListener("touchstart", function(e) {
+    this.startX = e.touches[0].pageX
+    this.startY = e.touches[0].pageY
   })
-  ele.addEventListener("touchmove", function (e) {
+  ele.addEventListener("touchmove", function(e) {
     this.isMove = true;
-		this.endX = e.touches[0].pageX
-		this.endY = e.touches[0].pageY
+    this.endX = e.touches[0].pageX
+    this.endY = e.touches[0].pageY
     this.distanceX = this.endX - this.startX
     this.distanceY = this.endY - this.startY
   })
-  ele.addEventListener("touchend", function () {
+  ele.addEventListener("touchend", function() {
     if (this.isMove) {
       this.isMove = false
-			//如果滑动距离太短
-			if(Math.abs(this.distanceX) < 2 && Math.abs(this.distanceY) < 2) {
-      return false;
-    }
-    let angle = Math.atan2(this.distanceY, this.distanceX) * 180 / Math.PI;
-    const EVENT = {
-      self: this,
-      distanceX: this.distanceX || 0,
-      distanceY: this.distanceY || 0,
-      startX: this.startX,
-      startY: this.startY,
-      endX: this.endX,
-      endY: this.endY
-    }
-    if(angle >= -45 && angle < 45) {
-      // console.log('向右')
-      option.right(EVENT)
-    }else if (angle >= 45 && angle < 135) {
-      // console.log('向下')
-      option.down(EVENT)
-    }else if (angle >= -135 && angle < -45) {
-      // console.log('向s上')
-      option.up(EVENT)
-    }
-    else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
-      // console.log('向左')
-      option.left(EVENT)
-    }
+      //如果滑动距离太短
+      if (Math.abs(this.distanceX) < 2 && Math.abs(this.distanceY) < 2) {
+        return false;
+      }
+      let angle = Math.atan2(this.distanceY, this.distanceX) * 180 / Math.PI
+      const EVENT = {
+        self: this,
+        distanceX: this.distanceX || 0,
+        distanceY: this.distanceY || 0,
+        startX: this.startX,
+        startY: this.startY,
+        endX: this.endX,
+        endY: this.endY
+      }
+      if (angle >= -45 && angle < 45) {
+        // console.log('向右')
+        option.right(EVENT)
+      } else if (angle >= 45 && angle < 135) {
+        // console.log('向下')
+        option.down(EVENT)
+      } else if (angle >= -135 && angle < -45) {
+        // console.log('向s上')
+        option.up(EVENT)
+      } else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
+        // console.log('向左')
+        option.left(EVENT)
+      }
     }
   })
 }
 
-new Promise((resolve) => {
-  Toast.success({
-    message: '您的企业认证资料更新正在审核中，请耐心等候！'
-  })
-  resolve()
-})
 
-
-new Promise((resolve) => {
-  Toast.success({
-    message: '您的企业认证资料更新正在审核中，请耐心等候！'
-  })
-  resolve()
-}).then(() => {
-  setTimeout(() => {
-    this.$router.push({
-      path: '/auth'
-    })
-  }, 1000)
-})
-
-
-function safelyResolveThen (self, then) {
+function safelyResolveThen(self, then) {
   let called = false
   try {
-    then(function (value) {
+    then(function(value) {
       if (called) {
         return
       }
       called = true
       doResolve(self, value)
-    }, function (error) {
+    }, function(error) {
       if (called) {
         return
       }
