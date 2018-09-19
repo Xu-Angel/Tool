2b5ca5ab57c12474823f2346e5f6098425d0b8d4
Angel <1049547382@qq.com>
2018-09-19 20:45:17 +0800

    adjust touch

--- touch.js
+++ touch.js
@@ -1,22 +1,46 @@
-function touch (ele,left,right) {
+function touch (ele,option) {
   ele.addEventListener("touchstart", function (e) {
-    this.startX = e.touches[0].pageX
-  });
+		this.startX = e.touches[0].pageX
+		this.startY = e.touches[0].pageY
+  })
   ele.addEventListener("touchmove", function (e) {
     this.isMove = true;
-    this.endX = e.touches[0].pageX;
-    this.distance = this.endX - this.startX
-  });
+		this.endX = e.touches[0].pageX
+		this.endY = e.touches[0].pageY
+    this.distanceX = this.endX - this.startX
+    this.distanceY = this.endY - this.startY
+  })
   ele.addEventListener("touchend", function () {
     if (this.isMove) {
       this.isMove = false
-        if (Math.abs(this.distance) > this.offsetWidth / 20) {
-            if (this.distance > 0) {
-              right();
-            } else {
-              left();
-            }
-        }
+			//如果滑动距离太短
+			if(Math.abs(this.distanceX) < 2 && Math.abs(this.distanceY) < 2) {
+      return false;
+    }
+    let angle = Math.atan2(this.distanceY, this.distanceX) * 180 / Math.PI;
+    const EVENT = {
+      self: this,
+      distanceX: this.distanceX || 0,
+      distanceY: this.distanceY || 0,
+      startX: this.startX,
+      startY: this.startY,
+      endX: this.endX,
+      endY: this.endY
+    }
+    if(angle >= -45 && angle < 45) {
+      // console.log('向右')
+      option.right(EVENT)
+    }else if (angle >= 45 && angle < 135) {
+      // console.log('向下')
+      option.down(EVENT)
+    }else if (angle >= -135 && angle < -45) {
+      // console.log('向s上')
+      option.up(EVENT)
+    }
+    else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
+      // console.log('向左')
+      option.left(EVENT)
+    }
     }
   })
 }
