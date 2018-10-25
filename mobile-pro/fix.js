b6b88c4385dc35c6566a22949f6e77af13f751b8
Angel <1049547382@qq.com>
2018-10-25 21:00:32 +0800

    update

--- mobile-pro/fix.js
+++ mobile-pro/fix.js
@@ -28,3 +28,49 @@
         }
     }
     addForEachToNodeList ()
+
+// https://tc39.github.io/ecma262/#sec-array.prototype.findIndex
+    if (!Array.prototype.findIndex) {
+        Object.defineProperty(Array.prototype, 'findIndex', {
+          value: function(predicate) {
+           // 1. Let O be ? ToObject(this value).
+            if (this == null) {
+              throw new TypeError('"this" is null or not defined');
+            }
+      
+            var o = Object(this);
+      
+            // 2. Let len be ? ToLength(? Get(O, "length")).
+            var len = o.length >>> 0;
+      
+            // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+            if (typeof predicate !== 'function') {
+              throw new TypeError('predicate must be a function');
+            }
+      
+            // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+            var thisArg = arguments[1];
+      
+            // 5. Let k be 0.
+            var k = 0;
+      
+            // 6. Repeat, while k < len
+            while (k < len) {
+              // a. Let Pk be ! ToString(k).
+              // b. Let kValue be ? Get(O, Pk).
+              // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+              // d. If testResult is true, return k.
+              var kValue = o[k];
+              if (predicate.call(thisArg, kValue, k, o)) {
+                return k;
+              }
+              // e. Increase k by 1.
+              k++;
+            }
+      
+            // 7. Return -1.
+            return -1;
+          }
+        });
+      }
+/*  如果您需要兼容不支持Object.defineProperty的JavaScript引擎，那么最好不要对Array.prototype方法进行 polyfill ，因为您无法使其成为不可枚举的。使用此方法需要注意你是否在uc浏览器环境,如果你的页面在支付宝上使用尤其注意,因为支付宝使用的就是uc浏览器环境. */
--- 数据操作技巧.js
+++ 数据操作技巧.js
@@ -32,6 +32,65 @@ let array1 = [1, "3", { a: 1}, 666];
 let copyArray = [ ...array1 ];
 console.log(copyArray) // [1, "3", {…}, 666]
 
+/* slice() 返回数组副本 */
+
+
+/* 数组过滤 false null 0 '' undefined NaN */
+//1.0 filter
+const compact = arr => arr.filter(Boolean)
+compact([0, 1, false, 2, '', 3, 'a', 'e' * 23, NaN, 's', 34]); // [ 1, 2, 3, 'a', 's', 34 ]
+
+/* 数组分块 */
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size)
+  )
+chunk([1, 2, 3, 4, 5], 2); // [[1,2],[3,4],[5]]
+
+
+/* 数组中某个值的出现次数 */
+const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0)
+countOccurrences([1, 1, 2, 1, 2, 3], 1); // 3
+
+/* 数组深度平铺 */
+
+//1.0 cancat , ...运算符, 递归
+const deepFlatten = arr => [].concat(...arr.map(v => Array.isArray(v) ? deepFlatten(v) : v))
+deepFlatten([1, [2], [[3], 4], 5]); // [1,2,3,4,5]
+
+/* 数组任意平铺 */
+
+//1.0 isArray 加上reduce 函数 扩展
+const flatten = (arr, depth = 1) => arr.reduce((a, v) => a.concat(depth > 1 && Array.isArray(v) ? flatten(v, depth -1) : v), [])
+flatten([1, [2], 3, 4]); // [1, 2, 3, 4]
+flatten([1, [2, [3, [4, 5], 6], 7], 8], 2); // [1, 2, 3, [4, 5], 6, 7, 8]
+
+/*  找出两数组中不同的值 */
+//1. Set数据结构中有 has()方法
+const difference = (a, b) => {
+  const s = new Set(b)
+  return a.filter(v => !s.has(v))
+}
+difference([1, 2, 3], [1, 2, 4]); // [3]
+
+/*  找出两数组中相同的值 */
+//1. Set数据结构中有 has()方法
+const common = (a, b) => {
+  const s = new Set(b)
+  return a.filter(v => s.has(v))
+}
+common([1, 2, 3], [1, 2, 4]); // [1, 2]
+
+/* 找出数组中匹配指定值的第一值的索引 */
+//1. findIndex
+const index = (arr, val) => arr.findIndex( v => v === val)
+index([1, 2, 4], 4)  // 2
+
+/* 找出数组中匹配指定值的所有索引 若为空则返回空数组*/
+//1. reduce() 方法
+const indexOfAll = (arr, val) => arr.reduce((pre, cur, i) => (cur === val ? [...pre, i] : pre), [])
+indexOfAll([1, 2, 3, 1, 2, 3], 1); // [0,3]
+indexOfAll([1, 2, 3], 4); // []
 
 /* 数组连接 */
 
@@ -52,6 +111,8 @@ const arr = [1, 1, 2, 2, 3]
 Array.from(new Set(arr))
 //1.1 ... === Array.form : ...运算符会把 Set 转换为 Array
 const arr = [...new Set(arr)]
+//2. 通过索引来判断
+arr.filter((v, i, arr)=> arr.indexOf(v) === arr.lastIndexOf(v))
 
 /* 删除对象中不需要的参数 || 抽取对象中某部分*/
 const Obj =  { boy1: "sunshine", boy2: "sunshine", girl1: "beautiful", girl2: "very beautiful", girl2: "very beautiful", girl2: "very very beautiful" }
@@ -79,8 +140,10 @@ var maxInNumbers = Math.max.apply(Math, numbers)
 var minInNumbers = Math.min.apply(Math, numbers)
 // => -85411
 
-
-
+/* 初始化一个二维数组 */
+//1.0 map方法
+const initialize2DArray = (w, h, val = null) => Array.from({ length: h}).map(() => Array.from({ length: w}).fill(val))
+initialize2DArray(2, 2, 0); // [[0,0], [0,0]]
 
 
 /*  将浮点数点左边的数每三位添加一个逗号，如12000000.11转化为『12,000,000.11 */
@@ -114,3 +177,20 @@ function round(v, p) {
 }
 // 使用
 round(123.456788, 2)  // 123.46
+
+/* forEach 右值循环 */
+const forEachRight = (arr, callback) =>
+  arr
+    .slice(0)
+    .reverse()
+    .forEach(callback)
+
+/* 数组自定义分组 */
+
+const groupBy = (arr, fn) =>
+  arr.map(typeof fn === 'function' ? fn : val => val[fn]).reduce((acc, val, i) => {
+    acc[val] = (acc[val] || []).concat(arr[i]);
+    return acc;
+  }, {});
+groupBy([6.1, 4.2, 6.3], Math.floor); // {4: [4.2], 6: [6.1, 6.3]}
+groupBy(['one', 'two', 'three'], 'length'); // {3: ['one', 'two'], 5: ['three']}
