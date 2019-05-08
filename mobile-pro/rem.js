ecc3e48149e1a5fc27c562c3bb54331da11055ab
Angel <1049547382@qq.com>
2019-05-08 19:06:59 +0800

    update about mobile

--- mobile-pro/rem.js
+++ mobile-pro/rem.js
@@ -13,3 +13,19 @@
   win.addEventListener(resizeEvt, recalc, false)
   doc.addEventListener('DOMContentLoaded', recalc, false)
 })(document, window)
+
+(function (doc, win) {
+  var docEl = doc.documentElement
+  var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
+
+  function recalc() {
+    var designWidth = 750
+    var clientWidth = docEl.clientWidth
+    if (!clientWidth || clientWidth > designWidth) return
+    docEl.style.fontSize = (100 * clientWidth / designWidth) + 'px'
+  }
+
+  if (!doc.addEventListener) return
+  win.addEventListener(resizeEvt, recalc, false)
+  doc.addEventListener('DOMContentLoaded', recalc, false)
+})(document, window)
