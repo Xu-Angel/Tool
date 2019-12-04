f297374ab1d4df628da6adefd6a377f23197c1f7
Angel <1049547382@qq.com>
2019-12-04 17:24:27 +0800

    短网址

--- 数据操作技巧1.js
+++ 数据操作技巧1.js
@@ -25,3 +25,33 @@ const obj = {
 };
 // obj => { a: 0, b: 1, d: 2 }
 
+/* 
+短网址函数
+*/
+function string10to62(number) {
+    var chars = '0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ'.split(''),
+      radix = chars.length,
+      qutient = +number,
+      arr = [];
+    do {
+      mod = qutient % radix;
+      qutient = (qutient - mod) / radix;
+      arr.unshift(chars[mod]);
+    } while (qutient);
+    return arr.join('');
+  }
+  
+  function string62to10(number_code) {
+    var chars = '0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ',
+      radix = chars.length,
+      number_code = String(number_code),
+      len = number_code.length,
+      i = 0,
+      origin_number = 0;
+    while (i < len) {
+      origin_number += Math.pow(radix, i++) * chars.indexOf(number_code.charAt(len - i) || 0);
+    }
+    return origin_number;
+  }
+  console.log(string10to62(string62to10('udtrgdp')), 0)
+  console.log(string62to10('udtrgdp'),1)
