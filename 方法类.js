ab5eef0f50afd6e16cafaeb54f289b0c0e4d5aa8
Xu-Angel <1049547382@qq.com>
2020-01-26 20:52:50 +0800

    neaten

--- 方法类.js
+++ 方法类.js
@@ -537,3 +537,17 @@ let mergePerson = mergeFrom.bind(null, a);
 mergePerson(b); // == b
 b = {};
 Object.assign(b, a); // == b
+
+/**
+ * 生成随机整数，范围是[min, max]
+ * @name randomInt
+ * @function
+ * @grammar randomInt(min, max)
+ *
+ * @param   {number} min    随机整数的最小值
+ * @param   {number} max    随机整数的最大值
+ * @return  {number}        生成的随机整数
+ */
+var randomInt = function(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
